2e4fca6386d7bf01816bc3d3c0e3eb698e7fcaa3
Felix Wang <felixwhwang@gmail.com>
2019-09-24 10:52:53 -0700

    update

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,24 @@
 
-function sumArray(){
-	
+function sumArray(array){
+  //use reduce built in function to accumulate the sum value
+	return array.reduce((acc, cur) => {
+    return acc + cur;
+  })
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, target){
+  var sum = array[0];
+  var result = [array[0]];
+  for(let i = 1; i < array.length; i++) {
+    if (sum === target) {
+      return result;
+    } else if (sum < target) {
+      sum += array[i];
+      result.push(array[i]);
+    } else {
+      fitWithinVal(array.slice(1), target);
+    }
+  }
 }
 
 function getAllNamesShorterThan(){
